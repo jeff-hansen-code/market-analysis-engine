331042cc439e6ba8d2d1f1a7e1f44cc33ea8ef22
Jeff Hansen <210188789+jeff-hansen-code@users.noreply.github.com>
2026-02-22 21:15:43 -0600

    Function app working now

--- infra/terraform/function_app.tf
+++ infra/terraform/function_app.tf
@@ -19,10 +19,5 @@ resource "azurerm_linux_function_app" "func" { #$ azurerm_windows_function_app  
   app_settings = local.app_settings
 }
 
-resource "azurerm_role_assignment" "func_kv_secrets_user" {
-  scope                = data.azurerm_key_vault.basekv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_linux_function_app.func.identity[0].principal_id
-}
 
 
