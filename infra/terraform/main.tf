c680e9b57580bb3501aa88a87c4064cccecf8294
Jeff Hansen <210188789+jeff-hansen-code@users.noreply.github.com>
2026-02-27 21:16:53 -0600

    tweaking terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,18 +4,6 @@ resource "azurerm_resource_group" "rg" {
   location = local.location
 }
 
-# Storage account for Functions / general use
-# NOTE: storage account name must be globally unique, all lowercase, 3–24 chars.
-resource "azurerm_storage_account" "funcsa" {
-  name                     = "mae${local.workspace}funcsa${local.name_suffix}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-
-
 resource "azurerm_service_plan" "plan" {
   name                = "mae-${local.workspace}-plan"
   resource_group_name = azurerm_resource_group.rg.name
